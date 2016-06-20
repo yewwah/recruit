7e47bc0a77dede3c03305d6a7e63f9265cdc76df
yewwah <yewwah10@gmail.com>
2016-06-20 10:54:05 +0800

    commiting

--- recruit_web/static/js/morris_data.js
+++ recruit_web/static/js/morris_data.js
@@ -61,17 +61,106 @@ var brexit = [{
         uniqueTweets:66700,
         exposure:203843
         }];
-data = [];
-fashion = [];
-iphone = [];
-impulserewards = [];
+var fashion = [{
+        period: '2016-06-10',
+        retweets: 100,
+        uniqueTweets:3000,
+        exposure:1253
+    }, {
+        period: '2016-06-11',
+        retweets: 460,
+        uniqueTweets:3800,
+        exposure:799
+    }, {
+        period: '2016-06-12',
+        retweets: 119200,
+        uniqueTweets:319400,
+        exposure:418885
+    }, {
+        period: '2016-06-13',
+        retweets: 106400,
+        uniqueTweets:26400,
+        exposure:856268
+    }, {
+        period: '2016-06-14',
+        retweets: 42400,
+        uniqueTweets:119900,
+        exposure:290949
+    }, {
+        period: '2016-06-15',
+        retweets: 20100,
+        uniqueTweets:66700,
+        exposure:203843
+        }];
+var iphone = [{
+        period: '2016-06-10',
+        retweets: 100,
+        uniqueTweets:3000,
+        exposure:1253
+    }, {
+        period: '2016-06-11',
+        retweets: 460,
+        uniqueTweets:3800,
+        exposure:799
+    }, {
+        period: '2016-06-12',
+        retweets: 119200,
+        uniqueTweets:319400,
+        exposure:418885
+    }, {
+        period: '2016-06-13',
+        retweets: 106400,
+        uniqueTweets:26400,
+        exposure:856268
+    }, {
+        period: '2016-06-14',
+        retweets: 42400,
+        uniqueTweets:119900,
+        exposure:290949
+    }, {
+        period: '2016-06-15',
+        retweets: 20100,
+        uniqueTweets:66700,
+        exposure:203843
+        }];
+var impulserewards = [{
+        period: '2016-06-10',
+        retweets: 100,
+        uniqueTweets:3000,
+        exposure:1253
+    }, {
+        period: '2016-06-11',
+        retweets: 460,
+        uniqueTweets:3800,
+        exposure:799
+    }, {
+        period: '2016-06-12',
+        retweets: 119200,
+        uniqueTweets:319400,
+        exposure:418885
+    }, {
+        period: '2016-06-13',
+        retweets: 106400,
+        uniqueTweets:26400,
+        exposure:856268
+    }, {
+        period: '2016-06-14',
+        retweets: 42400,
+        uniqueTweets:119900,
+        exposure:290949
+    }, {
+        period: '2016-06-15',
+        retweets: 20100,
+        uniqueTweets:66700,
+        exposure:203843
+        }];
 $('#tableId').find('tr').click( function(){
   var test = ($(this).index());
   $('#morris-area-chart').remove();
   $('#append').append('<div id =  "morris-area-chart" />');
   var chart = Morris.Area({
         element: 'morris-area-chart',
-        data: data,
+        data: orlando,
         xkey: 'period', //datetime 
         ykeys : ['retweets', 'uniqueTweets', 'exposure'], //tweets
         labels: ['Retweets', 'Unique Tweets', 'Hastag Exposure(00s)' ],
@@ -90,14 +179,11 @@ $('#tableId').find('tr').click( function(){
 
     }
     else if (test == '3'){
-        chart.setData(fashion);
-
-    }
-    else if (test == '4'){
         chart.setData(iphone);
 
     }
-    else if (test == '5'){
-    chart.setData(impulserewards);
-    };
+    else if (test == '4'){
+        chart.setData(impulserewards);
+
+    }
 });
