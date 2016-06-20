370b3aa394c2a531684df8cf7f62cd9dde69ccb6
yewwah <yewwah10@gmail.com>
2016-06-20 16:05:24 +0800

    modified the data to reflect true stats

--- recruit_web/static/js/morris_data.js
+++ recruit_web/static/js/morris_data.js
@@ -63,96 +63,96 @@ var brexit = [{
         }];
 var fashion = [{
         period: '2016-06-10',
-        retweets: 100,
-        uniqueTweets:3000,
-        exposure:1253
+        retweets: 9800,
+        uniqueTweets:41400,
+        exposure:11945
     }, {
         period: '2016-06-11',
-        retweets: 460,
-        uniqueTweets:3800,
-        exposure:799
+        retweets: 11800,
+        uniqueTweets:66400,
+        exposure:19541
     }, {
         period: '2016-06-12',
-        retweets: 119200,
-        uniqueTweets:319400,
-        exposure:418885
+        retweets: 14400,
+        uniqueTweets:55100,
+        exposure:16060
     }, {
         period: '2016-06-13',
-        retweets: 106400,
-        uniqueTweets:26400,
-        exposure:856268
+        retweets: 18000,
+        uniqueTweets:56000,
+        exposure:17129
     }, {
         period: '2016-06-14',
-        retweets: 42400,
-        uniqueTweets:119900,
-        exposure:290949
+        retweets: 18000,
+        uniqueTweets:56000,
+        exposure:17129
     }, {
         period: '2016-06-15',
-        retweets: 20100,
-        uniqueTweets:66700,
-        exposure:203843
+        retweets: 14400,
+        uniqueTweets:58000,
+        exposure:16637
         }];
 var iphone = [{
         period: '2016-06-10',
-        retweets: 100,
-        uniqueTweets:3000,
-        exposure:1253
+        retweets: 2700,
+        uniqueTweets:21800,
+        exposure:5190
     }, {
         period: '2016-06-11',
-        retweets: 460,
-        uniqueTweets:3800,
-        exposure:799
+        retweets: 4800,
+        uniqueTweets:52600,
+        exposure:16487
     }, {
         period: '2016-06-12',
-        retweets: 119200,
-        uniqueTweets:319400,
-        exposure:418885
+        retweets: 6800,
+        uniqueTweets:49200,
+        exposure:12503
     }, {
         period: '2016-06-13',
-        retweets: 106400,
-        uniqueTweets:26400,
-        exposure:856268
+        retweets: 6200,
+        uniqueTweets:52500,
+        exposure:9127
     }, {
         period: '2016-06-14',
-        retweets: 42400,
-        uniqueTweets:119900,
-        exposure:290949
+        retweets: 6800,
+        uniqueTweets:48900,
+        exposure:17878
     }, {
         period: '2016-06-15',
-        retweets: 20100,
-        uniqueTweets:66700,
-        exposure:203843
+        retweets: 4300,
+        uniqueTweets:52600,
+        exposure:10744  
         }];
 var impulserewards = [{
         period: '2016-06-10',
         retweets: 100,
-        uniqueTweets:3000,
-        exposure:1253
+        uniqueTweets:36000,
+        exposure:161
     }, {
         period: '2016-06-11',
-        retweets: 460,
-        uniqueTweets:3800,
-        exposure:799
+        retweets: 200,
+        uniqueTweets:61000,
+        exposure:573
     }, {
         period: '2016-06-12',
-        retweets: 119200,
-        uniqueTweets:319400,
-        exposure:418885
+        retweets: 100,
+        uniqueTweets:59800,
+        exposure:375
     }, {
         period: '2016-06-13',
-        retweets: 106400,
-        uniqueTweets:26400,
-        exposure:856268
+        retweets: 200,
+        uniqueTweets:60200,
+        exposure:223
     }, {
         period: '2016-06-14',
-        retweets: 42400,
-        uniqueTweets:119900,
-        exposure:290949
+        retweets: 400,
+        uniqueTweets:55600,
+        exposure:356
     }, {
         period: '2016-06-15',
-        retweets: 20100,
-        uniqueTweets:66700,
-        exposure:203843
+        retweets: 200,
+        uniqueTweets:62600,
+        exposure:331
         }];
 $('#tableId').find('tr').click( function(){
   var test = ($(this).index());
