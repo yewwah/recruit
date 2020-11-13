73fd30e6bd8dae74debeeca643f16d30fd6809d7
yewwah <yewwah10@gmail.com>
2020-11-13 12:04:48 +0800

    Update abc.js

--- abc.js
+++ abc.js
@@ -1,7 +1,16 @@
 // ==UserScript==
-// @name        Update and Download URL Metadata Test
-// @version     1
-// @updateURL   https://gist.github.com/raw/3906612/update-meta-test.meta.js
-// @downloadURL https://gist.github.com/raw/3906612/update-meta-test.user.js
+// @name         abc
+// @version      0.2.1
+// @description  try to take over the world!
+// @author       You
+// @match        http://*/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/yewwah/recruit/master/abc.js
 // ==/UserScript==
-alert('version 1 installed and running');
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var abc = 'abc'
+    })();
