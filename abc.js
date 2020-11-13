3c00ace2556a32d9c246f27a4966a09afeaae22d
yewwah <yewwah10@gmail.com>
2020-11-13 11:48:31 +0800

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Update and Download URL Metadata Test
+// @version     1
+// @updateURL   https://gist.github.com/raw/3906612/update-meta-test.meta.js
+// @downloadURL https://gist.github.com/raw/3906612/update-meta-test.user.js
+// ==/UserScript==
+alert('version 1 installed and running');
